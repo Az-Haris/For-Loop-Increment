9cac0ea3a4a296768ae0bc216c11865ed2863900
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-01-25 17:06:57 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int b;
+
+
+    for(b=1; b<=1000; b++)
+    {
+        printf("\tAz Haris %d\n",b);
+    }
+
+    getch();
+    return 0;
+}
